e00bde52f3be14a3bf8401656e726a5fded1fb96
sebTeam4 <sebastianmuramara@gmail.com>
2024-08-28 09:47:30 +0300

    add SQL file and comment API access code in index.js

---
+++ queries.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE notes (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(45),
+    date_read DATE,
+    notes VARCHAR(500),
+    rating INT,
+    user_id INT REFERENCES users(id)
+);
+
+SELECT * FROM notes
+JOIN users ON users.id = notes.user_id;
